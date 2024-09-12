9bc110db763a79fb8d150280e7d2a91140319df4
Ting Yun <ting.yun.career@gmail.com>
2024-09-12 11:21:12 -0700

    add kv database

---
+++ src/util/kv.ts
@@ -0,0 +1,12 @@
+import { kv } from "@vercel/kv";
+
+// kv is vercel's built-in key-value persistent storage.
+
+export async function kvSave(key: string, value: string) {
+  const status = await kv.set(key, value);
+  if (status !== "OK") throw Error("result not save");
+}
+
+export async function kvRead(key: string) {
+  return await kv.get(key);
+}
