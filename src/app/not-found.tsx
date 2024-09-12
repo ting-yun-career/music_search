6ebcb65a408000a340f887d07d8ce598d6be35b8
Ting Yun <ting.yun.career@gmail.com>
2024-09-12 01:19:49 -0700

    redirect

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function NotFound() {
+  const router = useRouter();
+  useEffect(() => {
+    const timer = setTimeout(function () {
+      clearTimeout(timer);
+      router.push("/");
+    }, 5000);
+  }, []);
+
   return (
     <>
-      <div>Opps this page does not exist</div>
+      <div>Opps this page does not exist. Redirecting you in 5 seconds</div>
     </>
   );
 }
