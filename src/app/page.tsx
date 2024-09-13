3ef9bc3a7183cb444993290442a72b81a9c54bf7
Ting Yun <ting.yun.career@gmail.com>
2024-09-13 11:54:46 -0700

    dropdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
           />
         </div>
         <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center">
-          <div className="w-3/4">
+          <div className="w-3/4 max-w-[600px]">
             <label
               className="block py-4 w-full text-center text-white font-semibold text-2xl"
               htmlFor="music-search"
--- src/component/search/index.tsx
+++ src/component/search/index.tsx
@@ -2,6 +2,7 @@
 
 import { useMemo, useState } from "react";
 import debounce from "lodash/debounce";
+import classNames from "classnames";
 
 interface Props {
   id: string;
@@ -13,14 +14,16 @@ export default function Search(props: Props) {
   const { id } = props;
 
   const [keyword, setKeyword] = useState("");
-  // const [isOpen, setOpen] = useState(false);
-  // const [result, setResult] = useState(null);
+  const [result, setResult] = useState<any>(null);
 
   const debouncedSearch = useMemo(
     () =>
       debounce(function (value: string) {
         if (value?.length >= 3) {
           console.log("searching for: ", value);
+          setResult("anything");
+        } else {
+          setResult(null);
         }
       }, 500),
     []
@@ -28,28 +31,57 @@ export default function Search(props: Props) {
 
   return (
     <>
-      <h1 className="text-white">{keyword}</h1>
-      <div className="flex justify-center">
-        <span className="w-[40px] bg-white text-gray-900 flex items-center justify-around rounded-l-[3px] cursor-pointer">
-          <span className="material-symbols-outlined text-[20px]">search</span>
-        </span>
-        <span className="flex-1 max-w-[500px]">
-          <input
-            type="text"
-            id={id}
-            className="w-full px-4 py-2 text-xs outline-1"
-            aria-label="Music Search"
-            placeholder="Search by artist or album"
-            value={keyword}
-            onChange={(e) => {
-              setKeyword(e.target.value);
-              debouncedSearch(e.target.value);
+      <div className="relative">
+        <div className="flex justify-center">
+          <span className="w-[40px] bg-white text-gray-900 flex items-center justify-around rounded-l-[3px] cursor-pointer">
+            <span className="material-symbols-outlined text-[20px]">
+              search
+            </span>
+          </span>
+          <span className="flex-1">
+            <input
+              type="text"
+              id={id}
+              className="w-full px-4 py-2 text-xs outline-1"
+              aria-label="Music Search"
+              placeholder="Search by artist or album"
+              value={keyword}
+              onChange={(e) => {
+                setKeyword(e.target.value);
+                debouncedSearch(e.target.value);
+              }}
+            />
+          </span>
+          <span
+            className="w-[40px] bg-white text-gray-900 flex items-center justify-center rounded-r-[3px] cursor-pointer"
+            onClick={(e) => {
+              setKeyword("");
+              setResult(null);
             }}
-          />
-        </span>
-        <span className="w-[40px] bg-white text-gray-900 flex items-center justify-center rounded-r-[3px] cursor-pointer">
-          <span className="material-symbols-outlined text-[20px]">close</span>
-        </span>
+          >
+            <span className="material-symbols-outlined text-[20px]">close</span>
+          </span>
+        </div>
+        <div
+          className={classNames(
+            { hidden: !result },
+            "absolute",
+            "top-[96%]",
+            "left-0",
+            "right-0",
+            "bg-gray-50",
+            "text-xs",
+            "rounded-b-[3px]",
+            "px-3",
+            "py-3",
+            "drop-shadow-md"
+          )}
+        >
+          <div className="text-gray-500 py-[3px]">Artists</div>
+          <div className="text-gray-900 py-[3px]">artist1</div>
+          <div className="text-gray-500 py-[3px]">Albums</div>
+          <div className="text-gray-900 py-[3px]">album1</div>
+        </div>
       </div>
     </>
   );
