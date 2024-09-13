f025facb343b57b52b72a9eb7398b0b4088b1c19
Ting Yun <ting.yun.career@gmail.com>
2024-09-12 22:20:30 -0700

    music search skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,36 @@
-import Header from "@/component/header";
+import Image from "next/image";
+import headerBg from "@/asset/header-background.jpg";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <div>Main</div>
+      <div className="relative h-[30vh] sm:h-[50vh]">
+        <div className="absolute top-0 bottom-0 left-0 right-0 brightness-50 overflow-hidden">
+          <Image
+            alt="Mountains"
+            src={headerBg}
+            className="w-full object-cover"
+            fill
+          />
+        </div>
+        <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center">
+          <div className="w-3/4">
+            <label
+              className="block py-4 w-full text-center text-white font-semibold text-2xl"
+              htmlFor="music-search"
+            >
+              Music Search
+            </label>
+            <input
+              type="text"
+              id="music-search"
+              className="w-full px-4 py-2 text-xs"
+              aria-label="Music Search"
+              placeholder="Search by artist or album"
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -5,8 +5,7 @@ import { search } from "@/actions/spotify";
 export default function Header() {
   return (
     <>
-      <div>Header</div>
-      <button
+      {/* <button
         className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
         onClick={() => {
           search("Taylor Siwft")
@@ -19,7 +18,7 @@ export default function Header() {
         }}
       >
         Search
-      </button>
+      </button> */}
     </>
   );
 }
