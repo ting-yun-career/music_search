dacdab34b2b435b22aeebc91cd8e5dd3af100dd7
Ting Yun <ting.yun.career@gmail.com>
2024-09-13 01:56:09 -0700

    music search input

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "material-symbols";
+import classNames from "classnames";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={classNames(inter.className, "antialiased")}>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,13 +21,27 @@ export default function Home() {
             >
               Music Search
             </label>
-            <input
-              type="text"
-              id="music-search"
-              className="w-full px-4 py-2 text-xs"
-              aria-label="Music Search"
-              placeholder="Search by artist or album"
-            />
+            <div className="flex justify-center">
+              <span className="w-[40px] bg-white text-gray-900 flex items-center justify-around rounded-l-[3px] cursor-pointer">
+                <span className="material-symbols-outlined text-[20px]">
+                  search
+                </span>
+              </span>
+              <span className="flex-1 max-w-[500px]">
+                <input
+                  type="text"
+                  id="music-search"
+                  className="w-full px-4 py-2 text-xs outline-1"
+                  aria-label="Music Search"
+                  placeholder="Search by artist or album"
+                />
+              </span>
+              <span className="w-[40px] bg-white text-gray-900 flex items-center justify-center rounded-r-[3px] cursor-pointer">
+                <span className="material-symbols-outlined text-[20px]">
+                  close
+                </span>
+              </span>
+            </div>
           </div>
         </div>
       </div>
