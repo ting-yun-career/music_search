7778773c61ea144110b735ca34f03535f1204967
Ting Yun <ting.yun.career@gmail.com>
2024-09-14 14:15:48 -0700

    placeholder text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
       <div className="relative h-[30vh] sm:h-[50vh]">
         <div className="absolute top-0 bottom-0 left-0 right-0 brightness-50 overflow-hidden">
           <Image
-            alt="Mountains"
+            alt="Home page background"
             src={headerBg}
             className="w-full object-cover"
             fill
@@ -30,7 +30,7 @@ export default async function Home() {
           </div>
         </div>
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-center pb-28">
         <div className="w-3/4 max-w-[600px] pt-[100px]">
           <FavouriteArtists favourites={favourites} />
         </div>
--- src/component/favouriteArtists/index.tsx
+++ src/component/favouriteArtists/index.tsx
@@ -14,17 +14,27 @@ export default function FavouriteArtists(props: Props) {
     <div className="border border-gray-300 rounded-[3px] px-5 py-6">
       <h2 className="text-xl font-bold">My Favourite Artists</h2>
       <div className="pt-5">
-        {artistIds.map((id) => (
-          <div key={id} className="flex justify-between">
-            <Link
-              className="underline decoration-dotted hover:text-pink-800"
-              href={`/artist/${id}`}
-            >
-              {favourites[id]}
-            </Link>
-            <Like isLiked={true} id={id} artistName={favourites[id]} />
-          </div>
-        ))}
+        {artistIds.length > 0 ? (
+          <>
+            {artistIds.map((id) => (
+              <div key={id} className="flex justify-between">
+                <Link
+                  className="underline decoration-dotted hover:text-pink-800"
+                  href={`/artist/${id}`}
+                >
+                  {favourites[id]}
+                </Link>
+                <Like isLiked={true} id={id} artistName={favourites[id]} />
+              </div>
+            ))}
+          </>
+        ) : (
+          <>
+            <div className="flex justify-center items-center min-h-[100px]">
+              You don't have any favourite artist
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
